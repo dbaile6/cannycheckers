527c2193d2afc6c49f2b981c12c67826daa861ab
Dylan Bailey <dylanba1@gmail.com>
2018-04-07 23:44:30 -0400

    Server working

--- server/main.js
+++ server/main.js
@@ -2,11 +2,13 @@
 
 import chalk from 'chalk';
 import express from 'express';
-const app = express();
 const path = require('path');
 
+const app = require('./app').default()
+
 const port = process.env.PORT || 4040;
-const server = app.listen(port, () => {
+const server = app.listen(port, (err) => {
+	if (err) throw err;
 	console.log(chalk.cyan(`Server listening on port ${port}.`))
 });
 const io = require('socket.io').listen(server);
